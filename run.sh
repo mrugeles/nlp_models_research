12a342728eb925970c1035592cb272dfc39c3458
Mario <mario.rugeles@endava.com>
2021-01-23 17:53:58 -0500

    add run script

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+pip install -U mlflow
+pip install -q tensorflow_addons
 python train_sklearn.py --algorithm RandomForestClassifier --sample_size 0.01 --tune_model false
 python train_sklearn.py --algorithm GradientBoostingClassifier --sample_size 0.01 --tune_model false
 python train_sklearn.py --algorithm GaussianNB --sample_size 0.01 --tune_model false
