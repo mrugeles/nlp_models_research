42e67bb898364860f53f1a8e87df0541b8265d1d
Mario <mario.rugeles@endava.com>
2021-01-23 18:26:27 -0500

    add runs

--- run_cnn.sh
+++ run_cnn.sh
@@ -1,5 +1,5 @@
 pip install -U mlflow
 pip install -q tensorflow_addons
-python train_tensorflow_cnn.py --algoritm cnn --sample_size 0.01
-python train_tensorflow_cnn.py --algoritm cnn --sample_size 0.1
-python train_tensorflow_cnn.py --algoritm cnn --sample_size 1
+python train_tensorflow_cnn.py --algorithm cnn --sample_size 0.01
+python train_tensorflow_cnn.py --algorithm cnn --sample_size 0.1
+python train_tensorflow_cnn.py --algorithm cnn --sample_size 1
