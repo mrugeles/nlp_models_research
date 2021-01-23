d500c68c6402959826dbd80ff520310834d98406
Mario <mario.rugeles@endava.com>
2021-01-23 18:51:20 -0500

    update datasets

---
+++ run_sklearn.bat
@@ -0,0 +1,12 @@
+python train_sklearn.py --algorithm RandomForestClassifier --sample_size 0.01 --tune_model false
+python train_sklearn.py --algorithm GradientBoostingClassifier --sample_size 0.01 --tune_model false
+python train_sklearn.py --algorithm GaussianNB --sample_size 0.01 --tune_model false
+python train_sklearn.py --algorithm SVC --sample_size 0.01 --tune_model false
+python train_sklearn.py --algorithm RandomForestClassifier --sample_size 0.1 --tune_model false
+python train_sklearn.py --algorithm GradientBoostingClassifier --sample_size 0.1 --tune_model false
+python train_sklearn.py --algorithm GaussianNB --sample_size 0.1 --tune_model false
+python train_sklearn.py --algorithm SVC --sample_size 0.1 --tune_model false
+python train_sklearn.py --algorithm RandomForestClassifier --sample_size 1 --tune_model false
+python train_sklearn.py --algorithm GradientBoostingClassifier --sample_size 1 --tune_model false
+python train_sklearn.py --algorithm GaussianNB --sample_size 1 --tune_model false
+python train_sklearn.py --algorithm SVC --sample_size 1 --tune_model false
